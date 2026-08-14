009d7217cf3f322ee74ea3d21d0b00638151532b
t84787704-hue <t84787704@gmail.com>
2026-08-14 14:40:23 +0300

    build: update application ID and version
    Change application ID to com.wasl.shop and bump version to 1.1.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,11 +14,11 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.wasl.saudishops"
+    applicationId = "com.wasl.shop"
     minSdk = 24
     targetSdk = 36
-    versionCode = 1
-    versionName = "1.0"
+    versionCode = 2
+    versionName = "1.1"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
