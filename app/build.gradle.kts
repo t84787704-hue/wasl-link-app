21d91153c0f1a8a9167f1320488f3ad418f26442
t84787704-hue <t84787704@gmail.com>
2026-08-19 21:07:51 +0300

    Update versionCode and versionName to 16 and 1.16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.wasl.saudishop"
     minSdk = 24
     targetSdk = 35
-    versionCode = 15
-    versionName = "1.15"
+    versionCode = 16
+    versionName = "1.16"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
