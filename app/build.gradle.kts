59486e05fd7050d6436af5b6a99c4d44540ee381
t84787704-hue <t84787704@gmail.com>
2026-08-19 21:30:48 +0300

    Update versionName from '1.16' to '2.0'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
     minSdk = 24
     targetSdk = 35
     versionCode = 16
-    versionName = "1.16"
+    versionName = "2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
