a35f2ab496dba900916be638d3d23a783f19db06
t84787704-hue <t84787704@gmail.com>
2026-08-19 18:02:14 +0300

    Bump versionCode to 15 and versionName to 1.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.wasl.saudishop"
     minSdk = 24
     targetSdk = 35
-    versionCode = 14
-    versionName = "1.14"
+    versionCode = 15
+    versionName = "1.15"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
