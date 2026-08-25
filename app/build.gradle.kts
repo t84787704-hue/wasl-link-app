e1efb3ab333489373b23b39b3aec0c771b5173e8
t84787704-hue <t84787704@gmail.com>
2026-08-25 14:03:30 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.wasl.saudishop"
     minSdk = 24
     targetSdk = 36
-    versionCode = 19
-    versionName = "2.1"
+    versionCode = 20
+    versionName = "2.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
